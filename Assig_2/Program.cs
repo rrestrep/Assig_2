f37842e4d3135d3f2e9fd31698fb2bcbdc800385
R Res <clonnes@hotmail.com>
2019-01-29 19:12:20 -0500

    Add project files.

---
+++ Assig_2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Assig_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
